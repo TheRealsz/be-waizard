1942b38d425b2169f18bf423facf5a1bf4ee513a
Robson <Robindiegoa@gmail.com>
2026-09-25 15:40:12 -0300

    :sparkles: feat: Enhance application startup by enabling shutdown hooks and retrieving the port from the configuration service for improved environment management.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Env } from './config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableShutdownHooks();
+
+  const config = app.get<ConfigService<Env, true>>(ConfigService);
+  await app.listen(config.get('PORT', { infer: true }));
 }
-bootstrap();
+void bootstrap();
